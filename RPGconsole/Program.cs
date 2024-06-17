9b7b172e4ce21d68107b68196fa33fbec565df3f
4Annk <smile.sir666@gmail.com>
2024-06-17 04:44:10 +0300

    test

---
+++ RPGconsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RPGconsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
